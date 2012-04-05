619b7a831c55b86628d061df2d2915790ff4caec
KeyboardCowboy <chris@162unlimited.com>
2012-04-04 23:25:04 -0600

    updating subtheme

--- template.php
+++ template.php
@@ -132,42 +132,49 @@ function theme_view_results_count($variables) {
  */
 function cbase_get_family_info($key = NULL) {
   global $theme;
+  $data = &drupal_static(__FUNCTION__);
+  //kpr($data);
 
-  $all_themes = list_themes();
-  $base_themes = array_keys((isset($all_themes[$theme]->base_themes) ? $all_themes[$theme]->base_themes : (isset($all_themes[$theme]->base_theme) ? array($all_themes[$theme]->base_theme => 0) : array())));
-  $theme_ancestry = array_merge($base_themes, array($theme));
-  $info = array();
+  // Check for cached data
+  if (!isset($data[$key])) {
+    $all_themes = list_themes();
+    $base_themes = array_keys((isset($all_themes[$theme]->base_themes) ? $all_themes[$theme]->base_themes : (isset($all_themes[$theme]->base_theme) ? array($all_themes[$theme]->base_theme => 0) : array())));
+    $theme_ancestry = array_merge($base_themes, array($theme));
+    $info = array();
 
-  // These keys are exclusive to each theme.  The value of the parent theme is
-  // irrevelent to the child theme, and so they must be explicitly defined in
-  // each child theme.
-  $overrides = array('regions', 'features', 'overlay_regions', 'regions_hidden');
+    // These keys are exclusive to each theme.  The value of the parent theme is
+    // irrevelent to the child theme, and so they must be explicitly defined in
+    // each child theme.
+    $overrides = array('regions', 'features', 'overlay_regions', 'regions_hidden');
 
-  // If the key is an overridden value, we don't need to combine all ancestral
-  // values, just return the active theme's value.
-  if (in_array($key, $overrides)) {
-    $info[$key] = $all_themes[$theme]->$key;
-  }
-  else {
-    foreach ($theme_ancestry as $_theme) {
-      if (!$key) {
-        $info[$_theme] = $all_themes[$_theme]->info;
-      }
-      elseif ($key == 'path') {
-        $info[$_theme] = drupal_get_path('theme', $_theme);
-      }
-      else {
-        if (isset($all_themes[$_theme]->info[$key]) && is_array($all_themes[$_theme]->info[$key])) {
-          $info[$_theme] = $all_themes[$_theme]->info[$key];
+    // If the key is an overridden value, we don't need to combine all ancestral
+    // values, just return the active theme's value.
+    if (in_array($key, $overrides)) {
+      $info[$key] = $all_themes[$theme]->$key;
+    }
+    else {
+      foreach ($theme_ancestry as $_theme) {
+        if (!$key) {
+          $info[$_theme] = $all_themes[$_theme]->info;
         }
-        elseif (isset($all_themes[$_theme]->info[$key]) && !is_array($all_themes[$_theme]->info[$key])) {
-          $info[$_theme] = $all_themes[$_theme]->info[$key];
+        elseif ($key == 'path') {
+          $info[$_theme] = drupal_get_path('theme', $_theme);
+        }
+        else {
+          if (isset($all_themes[$_theme]->info[$key]) && is_array($all_themes[$_theme]->info[$key])) {
+            $info[$_theme] = $all_themes[$_theme]->info[$key];
+          }
+          elseif (isset($all_themes[$_theme]->info[$key]) && !is_array($all_themes[$_theme]->info[$key])) {
+            $info[$_theme] = $all_themes[$_theme]->info[$key];
+          }
         }
       }
     }
+
+    $data[$key] = $info;
   }
 
-  return $info;
+  return $data[$key];
 }
 
 /**
