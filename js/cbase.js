9b46d53c63fbb06720aeec50202be855f8601eee
Chris Albrecht <chris@162unlimited.com>
2013-06-17 11:23:05 -0600

    beginning sass structure and merging js wil ost-js

--- js/cbase.js
+++ js/cbase.js
@@ -3,10 +3,103 @@
  * A Collection of common theming tools.
  */
 (function ($) {
-  $(document).ready(function() {
-    // Keep the admin toolbar below the admin menu.
-    cbase_adjust_admin_tools();
-  });
+  /**
+   * Global modifiers loaded into Drupal's behaviors.  They will be loaded into
+   * every page context.
+   */
+  Drupal.behaviors.cBase = {
+    // DOM Elements
+    e: {},
+
+    attach: function() {
+      this.adjustAdminMenus();
+    },
+
+    /**
+     * Push the Admin sidebar below the Admin Menu.
+     */
+    adjustAdminMenus: function() {
+      var self = this;
+      this.e.adminSidebar = $("#admin-toolbar .admin-blocks, #admin-toolbar .admin-toggle", context);
+      this.e.adminMenu = $("#admin-menu", context);
+
+      if (this.e.adminSidebar.length > 0) {
+        // If the admin menu is to be rendered on this page, listen for it to be
+        // added then adjust the sidebar.
+        if (Drupal.settings.admin_menu) {
+          toplisten = setInterval(function() {
+            // If the menu has loaded, slide the sidebar and stop the itterations.
+            if (self.e.adminMenu.length > 0) {
+              self.e.adminSidebar.animate({'top': self.e.adminMenu.inScrollView('top')});
+              clearInterval(toplisten);
+            }
+          }, 250);
+        }
+
+        $(window).scroll(function() {
+          if (self.e.adminMenu.length > 0) {
+            self.e.adminSidebar.css({'top': self.e.adminMenu.inScrollView('top')});
+          }
+        });
+      }
+    },
+  };
+
+  /**
+   * Create a fade-in/fade-out dialog box to display messages.
+   *
+   * Dependencies:
+   *   - isset();
+   */
+  cBaseDialog = {
+    e: $('<aside></aside>', {'id': 'cbase-dialog'}),
+    spinner: $('<span>Working...</span>'),
+    spinner_fa: $('<i></i>', {'class': 'icon-spinner icon-spin icon-2x'}),
+
+    // Initialize the dialog.
+    init: function() {
+      this.e.appendTo('body');
+
+      // Preload the spinner to prevent rendering delay.
+      this.spinner_fa.appendTo('body').addClass('element-invisible');
+    },
+
+    // Display (and optionally hide) the dialog.
+    show: function(content, delay) {
+      // Set the dialog contents.
+      this.e.html(content);
+
+      // Position the dialog in the center of the viewport.
+      var y = ($(window).height() / 2) - (this.e.height() / 2);
+      var x = ($(window).width() / 2) - (this.e.width() / 2);
+      this.e.css({'top': y, 'left': x});
+
+      // Show the dialog
+      this.e.fadeIn('fast');
+
+      // If a delay was provided, hide the dialog after the delay.
+      if (isset(delay)) {
+        this.hide(delay);
+      }
+    },
+
+    // Hide the dialog.
+    hide: function(delay) {
+      delay = delay || 0;
+      this.e.delay(delay).fadeOut('slow');
+    },
+
+    // Show the working image (font-awesome version).
+    working: function(delay) {
+      this.show(this.spinner_fa, delay);
+    }
+  };
+
+  /**
+   * Generic inline utility functions.
+   * These do not be instatiated on page load.
+   */
+  cBaseUtil = {};
 
   /**
    * Determine whether a given element is currently in the scrolled view.
@@ -34,7 +127,7 @@
     }
 
     return ((eB <= dB) && (dB - eB));
-  }
+  };
 
   /**
    * Set all elements in the range to the same height.
@@ -62,32 +155,40 @@
   };
 
   /**
-   * Push the Admin sidebar below the Admin Menu.
+   * Shorthand function to set element positioning.
    */
-  function cbase_adjust_admin_tools() {
-    var side = $("#admin-toolbar .admin-blocks, #admin-toolbar .admin-toggle");
+  $.fn.setPos = function(t,r,b,l) {
+    var pos = {};
+    var css = {};
 
-    if (side.length > 0) {
-      // If the admin menu is to be rendered on this page, listen for it to be
-      // added then adjust the sidebar.
-      if (Drupal.settings.admin_menu) {
-        toplisten = setInterval(function() {
-          var menu = $("#admin-menu");
+    pos.top    = t != 'undefined' ? t : null;
+    pos.right  = r != 'undefined' ? r : null;
+    pos.bottom = b != 'undefined' ? b : null;
+    pos.left   = l != 'undefined' ? l : null;
 
-          // If the menu has loaded, slide the sidebar and stop the itterations.
-          if (menu.length > 0) {
-            side.animate({'top': menu.inScrollView('top')});
-            clearInterval(toplisten);
-          }
-        }, 250);
+    for (var i in pos) {
+      if (pos[i] != null) {
+        css[i] = pos[i];
       }
-
-      $(window).scroll(function() {
-        var menu = $("#admin-menu");
-        if (menu.length > 0) {
-          side.css({'top': menu.inScrollView('top')});
-        }
-      });
     }
+
+    $(this).css(css);
+  };
+
+  /**
+   * Return the value of a URL query parameter.
+   */
+  function getQueryParam(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(location.search);
+    return results == null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  /**
+   * Shorthand function to mimic PHP's isset() function.
+   */
+  function isset(v) {
+    return (typeof v != 'undefined');
   }
 })(jQuery);
